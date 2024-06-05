5200ff5718946ef81cb2ed09b7f9b73820ad7ca0
emreylmzcom <delosemre@outlook.com>
2024-06-05 19:48:26 +0300

    chore: Cache categories for improved performance

--- envanter/views.py
+++ envanter/views.py
@@ -1,36 +1,42 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from django.core.cache import cache
+from django.conf import settings
 from .models import Kategori, Urun
 
+def get_cached_categories():
+    categories = cache.get('categories')  # cacheden datayi aliyorsun
+    if not categories:
+        categories = Kategori.objects.all()
+        cache.set('categories', categories, settings.CACHE_TIMEOUT)  # eger daha once cachelenmediyse cache atiyorsun
+    return categories
 
 def urun_listesi(request):
-    kategoriler = Kategori.objects.filter(parent__isnull=True)
+    kategoriler = get_cached_categories()
     urunler = Urun.objects.all()
     kategori_slug = request.GET.get('kategori_slug')
     if kategori_slug:
         urunler = urunler.filter(kategori__slug=kategori_slug)
     return render(request, 'envanter/urun_listesi.html', {'urunler': urunler, 'kategoriler': kategoriler})
 
-
 def kategori_urun_listesi(request, slug):
     kategori = get_object_or_404(Kategori, slug=slug)
     urunler = Urun.objects.filter(kategori=kategori)
-    kategoriler = Kategori.objects.all()
+    kategoriler = get_cached_categories()
     return render(request, 'envanter/kategori_urun_listesi.html', {'kategori': kategori, 'urunler': urunler, 'kategoriler': kategoriler})
 
 def urun_detay(request, slug):
     urun = get_object_or_404(Urun, slug=slug)
-    kategoriler = Kategori.objects.all()
+    kategoriler = get_cached_categories()
     return render(request, 'envanter/urun_detay.html', {'urun': urun, 'kategoriler': kategoriler})
 
 def anasayfa(request):
-    kategoriler = Kategori.objects.all()
+    kategoriler = get_cached_categories()
     return render(request, 'envanter/anasayfa.html', {'kategoriler': kategoriler})
 
 def iletisim(request):
-    kategoriler = Kategori.objects.all()
+    kategoriler = get_cached_categories()
     return render(request, 'envanter/iletisim.html', {'kategoriler': kategoriler})
 
 def hakkimizda(request):
-    kategoriler = Kategori.objects.all()
+    kategoriler = get_cached_categories()
     return render(request, 'envanter/hakkimizda.html', {'kategoriler': kategoriler})
--- stok_yonetimi/settings.py
+++ stok_yonetimi/settings.py
@@ -125,3 +125,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
+
+CACHE_TIMEOUT = 60 * 15  # 15 dakika
